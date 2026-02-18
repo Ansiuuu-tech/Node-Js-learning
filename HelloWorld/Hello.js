70285e82306c1f4be509a27b48a0f8e4bd413ae5
Anshu Gaur <anshugaur718@gmail.com>
2026-02-18 19:30:28 +0530

    Add initial implementation of Hello.js and math.js with basic arithmetic functions

---
+++ HelloWorld/Hello.js
@@ -0,0 +1,10 @@
+//console.log("Hey there! Welcome to the world of JavaScript!");
+
+const math = require('./math');
+// console.log("Math value is",math.add(2,5));
+// console.log("Math value is",math.sub(2,5));
+
+console.log("Math value is",math)
+
+const mat1=require("crypto");
+console.log(mat1)
