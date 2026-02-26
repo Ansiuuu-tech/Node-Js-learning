1a88fa63c28bbb39d5b8f4e554570bf0dc065c52
Anshu Gaur <anshugaur718@gmail.com>
2026-02-26 16:29:57 +0530

    Few modifications

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const http=require('http');
+// const http=require('http');
 
 // const myServer=http.createServer((req,res)=>{
 // console.log(req);
@@ -6,35 +6,51 @@ const http=require('http');
 // });
 // myServer.listen(8000,()=>{console.log("Server started!");})
 
-const fs=require('fs');
-const url=require('url');
+// const fs=require('fs');
+// const url=require('url');
 
-const myServer=http.createServer((req,res)=>{
-    if(req.url=="/favicon.ico"){
-       return res.end();
+// function myHandler(req,res){
+//     if(req.url=="/favicon.ico"){
+//        return res.end();
      
-    }
-    const log = `${new Date().toLocaleString()} ${req.url}\n`;
-    const myurl=url.parse(req.url);
-    console.log(myurl);
-    fs.appendFile('log.txt',log + "\n",(err,data)=>{
-        if(err){
-            console.log(err);
-        } })
-        switch(myurl.pathname){
-            case "/":
-                res.end("Welcome to Home Page!");
-                break;
-                case "/about":
-                    const username=myurl.querymyname;
-                    res.end("I'm anshu gaur(about page!");
-                    break;
-                    case "/contact":
-                        res.end("Contact me at: anshugaur@example.com");
-                    default:
-                        res.end("404 Not Found!");
-        }
+//     }
+//     const log = `${new Date().toLocaleString()}:${req.method} ${req.url} New request received\n`;
+//     const myurl=url.parse(req.url);
+//     console.log(myurl);
+//     fs.appendFile('log.txt',log + "\n",(err,data)=>{
+//         if(err){
+//             console.log(err);
+//         } })
+//         switch(myurl.pathname){
+//             case "/":
+//                 res.end("Welcome to Home Page!");
+//                 break;
+//                 case "/about":
+//                     const username=myurl.querymyname;
+//                     res.end("I'm anshu gaur(about page!");
+//                     break;
+//                     case "/contact":
+//                         res.end("Contact me at: anshugaur@example.com");
+//                     default:
+//                         res.end("404 Not Found!");
+//         }
    
-   // res.end("Hello from server again!"); //end the response and send data to client
+//    // res.end("Hello from server again!"); //end the response and send data to client
+// }
+
+
+// const myServer=http.createServer(app)
+
+// myServer.listen(3000,()=>{console.log("Server started yayy!");})
+
+const express=require("express")
+
+const app=express();
+
+app.get('/',(req,res)=>{
+    return res.send("hello from home page");
 })
-myServer.listen(3000,()=>{console.log("Server started yayy!");})
+app.get('/about',(req,res)=>{
+    return res.send("hello from about page "+"hey "+req.query.name);
+})
+app.listen(8000,()=>console.log("Server started"));
