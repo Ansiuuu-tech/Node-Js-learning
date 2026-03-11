e59e57ebb401a8dace8191ce9e0dbd7f8edc4601
Anshu Gaur <anshugaur718@gmail.com>
2026-03-11 20:26:18 +0530

    feat: Initialize Project 01 with Express server and user API
    - Added index.js to set up an Express server with routes for user management.
    - Implemented GET, POST, PATCH, and DELETE endpoints for user data.
    - Created package.json for project metadata and scripts.
    - Added task.txt outlining the REST API requirements and endpoints.
    - Updated package-lock.json to include express dependency.

---
+++ Project_01/index.js
@@ -0,0 +1,35 @@
+const express=require('express');
+const users=require("./MOCK_DATA.json");
+const app=express();
+const port=8000
+//routes
+// app.get("/users",(req,res)=>{
+//     const html=`
+//     <ul>
+//     ${users.map((user)=> `<li>${user.first_name}</li>`).join}
+//     </ul>`;
+//     res.send(html);
+// })
+
+app
+.route("/api/users/:id")
+.get((req,res) =>{
+    const id=Number(req.params.id);
+    const user = users.find((user)=> user.id==id)
+    return res.json(user)
+})
+.patch((req,res)=>{
+    //edit user with id
+    res.json({status:"pending"})
+})
+.delete((req,res)=>{
+     //delete user with id
+    res.json({status:"pending"})
+})
+app.post("/api/users",(req,res)=>
+{
+    //todo: create new user
+    return res.json({status : "pending"});
+})
+
+app.listen(port,()=> console.log(`Server started at port:${port}`))
